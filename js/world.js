453b400b68b2b1fa5a3199af6e207ac84f863ac8
MathisBullinger <bullinger.mathis@gmail.com>
2018-11-28 23:01:35 +0100

    fit world into screen and remove camera class

--- js/world.js
+++ js/world.js
@@ -2,13 +2,6 @@ import * as PIXI from 'pixi.js'
 import { renderer } from './graphics'
 import { Level } from './level'
 
-class Camera {
-  constructor(blocks_per_screen = 20) {
-    this.pos = new PIXI.Point(0, 0)
-    this.scale = new PIXI.Point(blocks_per_screen, window.innerHeight / window.innerWidth * blocks_per_screen)
-  }
-}
-
 class World {
   constructor() {
     this.Create()
@@ -16,10 +9,20 @@ class World {
 
   Create() {
     const lvl_data = require('../data/level/map_extern.json')
-    this.camera = new Camera(lvl_data.canvas.width / 32)
     this._CreateScene()
     this.level = new Level()
     this.level.Load(lvl_data, this.scene)
+
+    // rescale scene to fit into screen
+    const scene_ratio = Math.abs(this.scene.height / this.scene.width)
+    if (this.scene.width / window.innerWidth >= this.scene.height / window.innerHeight) {
+      this.scene.width = window.innerWidth / 2
+      this.scene.height = scene_ratio * this.scene.width * -1
+    } else {
+      this.scene.height = window.innerHeight / 2 * -1
+      this.scene.width = 1 / scene_ratio * this.scene.height * -1
+    }
+
   }
 
   Update(dt) {
@@ -40,7 +43,7 @@ class World {
     this.scene.position.y += window.innerHeight / renderer.resolution
     this.scene.scale.y *= -1
     // scale
-    this.pix_per_unit = window.innerWidth / this.camera.scale.x
+    this.pix_per_unit = window.innerWidth / 10
     this.scene.scale.x *= this.pix_per_unit / renderer.resolution
     this.scene.scale.y *= this.pix_per_unit / renderer.resolution
   }
