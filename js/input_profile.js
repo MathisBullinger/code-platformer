d2a02500eb34e00eccdb02b3180b0c5cbbdc144b
KukiCODE <kunal.kirpalani@code.berlin>
2018-12-10 17:26:41 +0100

    Merge dev

---
+++ js/input_profile.js
@@ -0,0 +1,76 @@
+import { Keyboard as key, Gamepad, Mouse } from './interaction'
+import { Vec2D } from './math'
+import { renderer } from './graphics'
+
+class InputKeyboard {
+  Init(player) {
+    this._player = player
+    // arrow keys
+    key.BindKey('ArrowLeft', dt => player.MoveLeft(dt))
+    key.BindKey('ArrowRight', dt => player.MoveRight(dt))
+    key.BindKey('ArrowUp', dt => player.Jump(dt), true)
+    // wasd
+    key.BindKey('A', dt => player.MoveLeft(dt))
+    key.BindKey('D', dt => player.MoveRight(dt))
+    key.BindKey('W', dt => player.Jump(dt), true)
+    // dash
+    key.BindKey('Shift', () => player.Dash(), true)
+  }
+
+  Update() {
+    if (Mouse.IsDown()) this._player.Attack()
+  }
+
+  // get view direction relative to position
+  GetViewDir(obj) {
+    // Get screen coordinates (bottom-left based) for the mouse
+    const screen_mouse_pos = this._GetMousePos()
+    // Get screen coordinates (bottom-left based) for the weapon holster
+    const screen_holster_pos = new Vec2D(
+      obj.graphic.parent.worldTransform.tx,
+      window.innerHeight - obj.graphic.parent.worldTransform.ty)
+    // Get the distance between the two => direction vector
+    const distance = Vec2D.Sub(screen_mouse_pos, screen_holster_pos)
+    return Vec2D.Div(distance, distance.Magnitude) // normalize
+  }
+
+  _GetMousePos() {
+    return new Vec2D(
+      renderer.plugins.interaction.mouse.global.x,
+      window.innerHeight - renderer.plugins.interaction.mouse.global.y
+    )
+  }
+}
+
+class InputGamepad {
+  Init(player) {
+    this._player = player
+
+    // move
+    Gamepad.BindInput('stick_left_x', (dt, value) => {
+      if (value > 0) this._player.MoveRight(dt)
+      else this._player.MoveLeft(dt)
+    })
+    // jump
+    Gamepad.BindInput('A', dt => this._player.Jump(dt), true)
+    Gamepad.BindInput('LB', dt => this._player.Jump(dt), true)
+    // dash
+    Gamepad.BindInput('RB', () => this._player.Dash(), true)
+    // attack
+    Gamepad.BindInput('RT', () => this._player.Attack())
+  }
+
+  Update() {
+
+  }
+
+  GetViewDir() {
+    const stick_dir = Gamepad.GetStick('right')
+    if (stick_dir.x || stick_dir.y)
+      return new Vec2D(stick_dir.x, stick_dir.y * -1)
+    else
+      return null
+  }
+}
+
+export { InputKeyboard, InputGamepad }
---
+++ js/ui/user_interface.js
@@ -0,0 +1,24 @@
+import * as PIXI from 'pixi.js'
+import { Level } from './../level'
+import { PlayerUI } from './player_ui'
+
+class UI {
+
+  constructor() {
+    this.graphic = new PIXI.Container()
+    this._setLevel(Level.ActiveLevel)
+  }
+
+  Update() {
+    if (this._current_level !== Level.ActiveLevel) this._setLevel(Level.ActiveLevel)
+    this._player_ui.Update()
+  }
+
+  _setLevel(lvl) {
+    this._current_level = lvl
+    this._player_ui = new PlayerUI(this._current_level._players)
+    this.graphic.addChild(this._player_ui.graphic)
+  }
+}
+
+export { UI }
