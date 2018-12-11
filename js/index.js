e6ead7534c758c70b5c44fe7a4f2ca9b8593ee28
Mathis Bullinger <bullinger.mathis@gmail.com>
2018-12-11 19:51:46 +0100

    add url param to directly enter game

--- js/index.js
+++ js/index.js
@@ -1,19 +1,26 @@
 import '../style/master.scss'
-import { Game } from './game'
-import { Mainmenu } from './mainmenu'
+import {Game} from './game'
+import {Mainmenu} from './mainmenu'
+import {GetUrlParam} from './util'
 
 // disable Parcel.js HMR
 if (module.hot) {
-  module.hot.dispose(function () {
+  module.hot.dispose(function() {
     window.location.reload()
   })
 }
 
-// shwo main menu
-const menu = new Mainmenu()
-menu.Show()
-menu.OnStart(() => {
-  // start game
+// directly start game if "state=game"
+if (GetUrlParam('state') == 'game') {
   let game = new Game()
   game.Start()
-})
+} else {
+  // shwo main menu
+  const menu = new Mainmenu()
+  menu.Show()
+  menu.OnStart(() => {
+    // start game
+    let game = new Game()
+    game.Start()
+  })
+}
