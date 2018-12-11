1cf92415ca4bd6c72a6274ee27a9ef9b0e7149ab
Mathis Bullinger <bullinger.mathis@gmail.com>
2018-12-11 19:46:28 +0100

    add main menu

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 import '../style/master.scss'
 import { Game } from './game'
+import { Mainmenu } from './mainmenu'
 
 // disable Parcel.js HMR
 if (module.hot) {
@@ -8,6 +9,11 @@ if (module.hot) {
   })
 }
 
-// start game
-let game = new Game()
-game.Start()
+// shwo main menu
+const menu = new Mainmenu()
+menu.Show()
+menu.OnStart(() => {
+  // start game
+  let game = new Game()
+  game.Start()
+})
